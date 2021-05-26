f4b8610ee093c1d377fa4b5e875a55c575b01a55
Scott Rowan <scott.michael.rowan@gmail.com>
2021-05-26 17:13:59 +0100

    simple/advanced structure

---
+++ Chapter 2 - Kinematics/Section 2.1 - Displacement/beginner.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2021, srowanphys
+# All rights reserved.
+
+# This source code is licensed under the Apache 2.0 license found in the
+# LICENSE.md file in the root directory of this source tree.
+
+# The following code constructs an interactive program for calculating displacement
+# The program is written in the Python language
+# All lines starting with a '#' symbol are so-called 'comments' and are not part of the program, but rather just helpful notes for the reader
+
+# You may change the values of the inital and/or final position variables to change the calculated displacement
+
+initial_position = 2
+final_position = -10
+
+displacement = final_position - initial_position
+
+print("Displacement:", displacement, "m")
